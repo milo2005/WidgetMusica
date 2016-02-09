ea5fe11595aea37c384104422bb68434aeccfd1c
Dario Chamoro <dario.chamorro.v@gmail.com>
2016-02-09 15:47:02 -0500

    Servicio de musica implementado

--- app/src/main/java/com/example/estacionvl_tc_014/widgetmusica/services/MusicService.java
+++ app/src/main/java/com/example/estacionvl_tc_014/widgetmusica/services/MusicService.java
@@ -1,11 +1,17 @@
 package com.example.estacionvl_tc_014.widgetmusica.services;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 
-public class MusicService extends Service {
+import java.io.IOException;
+
+public class MusicService extends Service implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener {
 
     public static final String ACTION_PLAY="musicservice.play";
     public static final String ACTION_PAUSE="musicservice.pause";
@@ -29,7 +35,7 @@ public class MusicService extends Service {
     private void playMusic() {
         if(player == null){
             initMediaPlayer();
-            loadSong("");
+            loadSong("revelations.mp3");
             setForeground();
         }else{
             if(paused)
@@ -39,14 +45,35 @@ public class MusicService extends Service {
     }
 
     private void initMediaPlayer() {
-
+        player = new MediaPlayer();
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setOnCompletionListener(this);
+        player.setOnPreparedListener(this);
     }
 
     private void loadSong(String song) {
+        try {
 
+            AssetFileDescriptor fD = getAssets().openFd("musica/"+song);
+
+            player.setDataSource(fD.getFileDescriptor(),
+                    fD.getStartOffset(), fD.getLength()
+                    );
+
+            player.prepareAsync();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void setForeground() {
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("Reproduciendo")
+                .setContentText("Revelations")
+                .setSmallIcon(android.R.drawable.ic_media_play)
+                .build();
+        startForeground(101,notification);
 
     }
 
@@ -67,5 +94,23 @@ public class MusicService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
        return null;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        stopSelf();
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        player.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        player.release();
+        player = null;
+        stopForeground(true);
+        super.onDestroy();
     }
 }
